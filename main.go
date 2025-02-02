33aa0666d44e7abc0374813d6ef48b680df0446a
fhiroki <hiroki976@gmail.com>
2025-02-02 14:03:35 +0900

    Try different avatar implementations

--- main.go
+++ main.go
@@ -16,7 +16,11 @@ import (
 	"github.com/markbates/goth/providers/google"
 )
 
-var avatars Avatar = UseFileSystemAvatar
+var avatars Avatar = TryAvatars{
+	UseFileSystemAvatar,
+	UseAuthAvatar,
+	UseGravatar,
+}
 
 type templateHandler struct {
 	once     sync.Once
